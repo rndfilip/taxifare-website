72654d90492e5c57a7d06297107f1e2bec4395b3
Filip Vykydal <vykydalf@gmail.com>
2023-11-24 16:25:57 +0000

    test

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import pandas as pd
+# import pandas as pd
 import requests
+import datetime
 
 '''
 # TaxiFareModel front
@@ -9,16 +10,17 @@ st.title('Taxi Fare Prediction')
 
 st.sidebar.header('Input Parameters')
 
-pickup_datetime = st.date_input('Pickup Date', value=None)
-pickup_longitude = st.sidebar.number_input('Pickup Longitude', value=0.0)
-pickup_latitude = st.sidebar.number_input('Pickup Latitude', value=0.0)
-dropoff_longitude = st.sidebar.number_input('Dropoff Longitude', value=0.0)
-dropoff_latitude = st.sidebar.number_input('Dropoff Latitude', value=0.0)
-passenger_count = st.sidebar.number_input('Passenger Count', value=1)
+pickup_datetime = st.date_input('Pickup Date',datetime.date(2023, 12, 12))
+time_input = st.time_input('bullshit need', datetime.time(8, 45))
+pickup_longitude = st.sidebar.number_input('Pickup Longitude', value=75.88)
+pickup_latitude = st.sidebar.number_input('Pickup Latitude', value=75.12)
+dropoff_longitude = st.sidebar.number_input('Dropoff Longitude', value=77.12)
+dropoff_latitude = st.sidebar.number_input('Dropoff Latitude', value=77.83)
+passenger_count = st.sidebar.number_input('Passenger Count', value=2)
 
 # Prepare input data for prediction
 input_data = {
-    'pickup_datetime': str(pickup_datetime),
+    'pickup_datetime': datetime.datetime.combine(pickup_datetime,time_input).strftime("%Y-%m-%d %H:%M:%S"),
     'pickup_longitude': pickup_longitude,
     'pickup_latitude': pickup_latitude,
     'dropoff_longitude': dropoff_longitude,
@@ -35,21 +37,9 @@ url = 'https://taxifare.lewagon.ai/predict'
 response = requests.get(url, params= input_data)
 st.write(response.url)
 
-st.write(response)
 
-# Check if the request was successful
-# if response.status_code == 200:
-#     # Extract the prediction from the response
-#     prediction = response.json().get('prediction')
-#     # Display the predicted fare
-#     st.markdown('## Prediction')
-#     st.write(f'Predicted Fare: ${prediction:.2f}')
-# else:
-#     st.error(f"Error making prediction. Status code: {response.status_code}")
-#     st.text(response.text)  # Display the error response for debugging
-# Display the prediction
 st.markdown('## Prediction')
-st.write(response.json())
+st.write("Fare ride price",response.json()["fare"])
 st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
 '''
